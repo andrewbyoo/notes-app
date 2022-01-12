9edf09865ff0c4621e768fd2afe414fd10ef3c36
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-12 17:51:10 -0500

    Added paths to the index.html and notes.html

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
 
-app.listen(PORT, () =>
-  console.log(`Static asset routes at http://localhost:${PORT}`))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.listen(PORT, () => console.log(`Static asset routes at http://localhost:${PORT}`));
