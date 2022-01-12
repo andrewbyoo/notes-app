7948d813bfc40057c9fd593afa1c8810553cbdea
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-12 18:04:20 -0500

    Fixed linking to db.json file, changed app.get * to / as it cause issues with get on other paths, and added get on the db.json

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const db = require('./db/db.json');
+const db = require('./db/db');
 const path = require('path');
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+// When using * instead of /, it would only give me index.html whenever i try to get the /api/notes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+  console.log(`${req.method} api notes`);
+  return res.json(db);
+})
 
 app.listen(PORT, () => console.log(`Static asset routes at http://localhost:${PORT}`));
