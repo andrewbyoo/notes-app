ffa3a017af2a55b2cfaa20ff14014d79c7be7efa
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-13 16:42:47 -0500

    Moved express static public after the urlencoded, added express json, and added console log for post to /api/notes

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(db));
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body)
+})
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
