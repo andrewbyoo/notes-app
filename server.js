49982f13d1179369bcfd223b2f6a07db8aef52ac
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-13 18:29:35 -0500

    Changed object key from note_id to id

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/api/notes', (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuid()
+      id: uuid()
     };
 
     fs.readFile('./db/db.json', (err, data) => {
