70ef72d8fefd5e2b449083603714e6f712e272ad
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-13 16:49:22 -0500

    Added require for uuid helper js file, added deconstructed req.body, defined new variable if the title and text fields of the html have any value, and added uuid function to the newNote object

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db/db');
 const path = require('path');
+const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +16,15 @@ app.get('/api/notes', (req, res) => res.json(db));
 
 app.post('/api/notes', (req, res) => {
   console.log(req.body)
+  const {title, text} = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      review_id: uuid()
+    };
+  }
 })
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
