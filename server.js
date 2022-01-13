973065ae52192f0463fc445f4b11acc4d9e99f55
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-13 14:22:03 -0500

    Added get for / for testing heroku issue

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.port || 3001;
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(db));
