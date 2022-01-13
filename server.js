567cdc622374a90961793abc3e18f8e53671aee8
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-13 14:50:04 -0500

    Fixed port and removed duplicate path

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const db = require('./db/db');
 const path = require('path');
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
