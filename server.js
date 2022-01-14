373a68c229a8fdb45b632d8a1ef862e3d936cb60
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-14 09:35:11 -0500

    Changed /api/notes get to return an fs.readFile

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.use(express.static('public'));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(db));
+app.get('/api/notes', (req, res) => fs.readFile('./db/db.json', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const parsedData = JSON.parse(data);
+  res.json(parsedData);
+}));
 
 app.post('/api/notes', (req, res) => {
   const {title, text} = req.body;
